968ccfb7b50de8b25446fa269987d7f3e0fd2b32
kunalbarot3188 <76148075+kunalbarot3188@users.noreply.github.com>
2024-04-05 20:14:45 -0400

    Update sonarPipeline.groovy

--- vars/sonarPipeline.groovy
+++ vars/sonarPipeline.groovy
@@ -1,40 +1,43 @@
-pipeline {
-    agent any
-    
-    environment {
-        SONAR_RUNNER_HOME = tool name: 'SonarScanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-    }
-    
-    stages {
-        stage('Clean workspace') {
-            steps {
-                cleanWs()
-            }
-        }
+def call(Map config) {
+    def projectName = config.projectName
+    def gitRepoUrl = config.gitRepoUrl
+    def sonarToken = config.sonarToken
 
-        stage('Checkout from Git') {
-            steps {
-                git branch: 'main', url: 'https://github.com/my-user/my-repo.git'
-            }
-        }
-
-        stage('Sonarqube Analysis') {
-            steps {
-                withSonarQubeEnv('sonar-server') {
-                    sh "${SONAR_RUNNER_HOME}/bin/sonar-scanner -Dsonar.projectName='my-project' -Dsonar.projectKey='my-project'"
+    pipeline {
+        agent any
+        
+        stages {
+            stage('Clean workspace') {
+                steps {
+                    cleanWs()
                 }
             }
-        }
 
-        stage('Quality Gate') {
-            steps {
-                script {
-                    waitForQualityGate abortPipeline: false, credentialsId: 'my-sonar-token'
+            stage('Checkout from Git') {
+                steps {
+                    git branch: 'main', url: gitRepoUrl
+                }
+            }
+
+            stage('Sonarqube Analysis') {
+                steps {
+                    withSonarQubeEnv('sonar-server') {
+                        sh "${SONAR_RUNNER_HOME}/bin/sonar-scanner -Dsonar.projectName='${projectName}' -Dsonar.projectKey='${projectName}'"
+                    }
+                }
+            }
+
+            stage('Quality Gate') {
+                steps {
+                    script {
+                        waitForQualityGate abortPipeline: false, credentialsId: sonarToken
+                    }
                 }
             }
         }
     }
 }
+
 
 
 
